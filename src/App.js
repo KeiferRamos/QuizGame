7ad44457b5aff36761004596317c79c496fe3dce
KeiferRamos <keiferramos2001@gmail.com>
2022-02-18 13:40:34 +0800

    create quiz game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,158 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useContext, useEffect, useState } from "react";
+import "./style.css";
+import Question from "./Data";
+
+const GlobalContext = createContext({});
+
+export const AppProvider = ({ children }) => {
+  const [index, setIndex] = useState(0);
+  const [time, setTime] = useState(10);
+  const [answer, setAnswer] = useState("");
+  const [startGame, setStartGame] = useState(false);
+  const [points, setPoints] = useState(0);
+  return (
+    <GlobalContext.Provider
+      value={{
+        index,
+        setIndex,
+        time,
+        setTime,
+        answer,
+        setAnswer,
+        setStartGame,
+        startGame,
+        setPoints,
+        points,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
 
 function App() {
+  const { index, setTime, time, answer, startGame } = useContext(GlobalContext);
+
+  useEffect(() => {
+    if (time > 0) {
+      const timer = setTimeout(() => {
+        setTime((prev) => (prev -= 1));
+      }, 1000);
+      return () => clearTimeout(timer);
+    }
+  }, [time]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="main-container">
+      {index < Question.length ? (
+        startGame ? (
+          !answer && time > 0 ? (
+            <QandA />
+          ) : (
+            <Next />
+          )
+        ) : (
+          <StartGame />
+        )
+      ) : (
+        <EndGame />
+      )}
+    </div>
+  );
+}
+
+function StartGame() {
+  const { setStartGame, setTime } = useContext(GlobalContext);
+
+  const start = () => {
+    setStartGame(true);
+    setTime(10);
+  };
+
+  return (
+    <div className="start-game container">
+      <div>
+        <p>Quiz Game</p>
+        <button onClick={() => start()}>Start Game</button>
+      </div>
+    </div>
+  );
+}
+
+function EndGame() {
+  const { points, setIndex, setTime, setAnswer, setPoints } =
+    useContext(GlobalContext);
+
+  function playAgain() {
+    setIndex(0);
+    setTime(10);
+    setAnswer("");
+    setPoints(0);
+  }
+  return (
+    <div className="end-game container">
+      <div>
+        <p>That's all</p>
+        <p>thank you for playing</p>
+        <button onClick={() => playAgain()}>Play Again</button>
+        <p style={{ margin: "20px" }}>Score: {points}</p>
+      </div>
+    </div>
+  );
+}
+
+function QandA() {
+  const { setAnswer, time, index, setPoints } = useContext(GlobalContext);
+
+  const addPoints = (item) => {
+    if (item == Question[index].answer) {
+      setPoints((prev) => prev + 1);
+    }
+    setAnswer(item);
+  };
+  return (
+    <div>
+      <div className="questionAndtime">
+        <div className="question">{Question[index].question}</div>
+        <div className="time">{time}</div>
+      </div>
+      <div className="timer"></div>
+      <div className="choices">
+        {Question[index].choices.map((item, i) => {
+          return (
+            <div key={i} onClick={() => addPoints(item)}>
+              {item}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+function Next() {
+  const { index, setIndex, setTime, answer, setAnswer } =
+    useContext(GlobalContext);
+  const next = () => {
+    setIndex(index + 1);
+    setTime(10);
+    setAnswer("");
+  };
+  return (
+    <div className="times-up container">
+      <div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {answer
+            ? answer == Question[index].answer
+              ? "Nice You're Correct"
+              : "Opps That's Wrong"
+            : "Time's up"}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>answer: {Question[index].answer}</p>
+        <button className="next-btn" onClick={next}>
+          next
+        </button>
+      </div>
     </div>
   );
 }
---
+++ src/Data.js
@@ -0,0 +1,54 @@
+export default [
+  {
+    question: "What year was the very first model of the iPhone released?",
+    answer: 2007,
+    choices: [2001, 2009, 2011, 2007],
+  },
+  {
+    question: "What’s the shortcut for the “copy” function on most computers?",
+    answer: "ctrl c",
+    choices: ["ctrl c", "alt tab", "ctrl v", "shift 1"],
+  },
+  {
+    question: "What is often seen as the smallest unit of memory?",
+    answer: "kilobyte",
+    choices: ["megabyte", "terabyte", "kilobyte", "gigabyte"],
+  },
+  {
+    question: "Java is a type of OS",
+    answer: "false",
+    choices: ["false", "true"],
+  },
+  {
+    question: "“HTTP” stand for HyperText Transfer Prototype",
+    answer: "false",
+    choices: ["false", "true"],
+  },
+  {
+    question: "What is the name of the man who launched eBay back in 1995?",
+    answer: "Pierre Omidyar",
+    choices: ["Corvus Glaive", "Ebony Maw", "Pierre Omidyar", "Cull Obsidian"],
+  },
+  {
+    question: "Hotmail is owned by Microsoft?",
+    answer: "true",
+    choices: ["false", "true"],
+  },
+  {
+    question: "Google Chrome, and Explorer are different types of what?",
+    answer: "Web browsers",
+    choices: ["software", "search engine", "Web browsers", "none of the above"],
+  },
+  {
+    question: "Twitter’s original name is twttr",
+    answer: "true",
+    choices: ["true", "false"],
+  },
+  {
+    question: "CSS stands for Cascading Style Sheets",
+    answer: "true",
+    choices: ["true", "false"],
+  },
+];
+
+export const source = "https://www.opinionstage.com/blog/trivia-questions/";
